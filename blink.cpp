c4cc36c5e0cc0b613afea1b8cbcc34dd8e13d415
Ygor Seixas <57631429+SeixasYgor@users.noreply.github.com>
2022-05-20 07:44:37 -0400

    Update blink.cpp

--- blink.cpp
+++ blink.cpp
@@ -2,12 +2,15 @@ int led = 13;
 
 void setup () {
     pinMode(led, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop () {
     digitalWrite(led, HIGH);
+    Serial.println("Led HIGH");
     delay(2000);
 
     digitalWrite(led, LOW);
+    Serial.println("Led LOW");
     delay(500);
 }
